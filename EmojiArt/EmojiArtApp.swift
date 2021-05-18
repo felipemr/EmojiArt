7f7d93278a639f9965417f4521745115cc876275
Felipe Marques Ramos <felipemr94@outlook.com>
2021-05-17 23:08:16 -0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Felipe Marques Ramos on 17/05/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
