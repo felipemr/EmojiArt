3350a8985439e2d48602427188e5c3adb8b597a9
Felipe Marques Ramos <felipemr94@outlook.com>
2021-05-22 19:39:28 -0300

    DoubleTap to fit

--- EmojiArt/View/DocumentView.swift
+++ EmojiArt/View/DocumentView.swift
@@ -25,11 +25,16 @@ struct DocumentView: View {
             
             GeometryReader{ geometry in
                 ZStack{
-                    Color.gray.overlay(OptionalImage(uiImage: document.backgroundImage))
+                    Color.gray.overlay(
+                        OptionalImage(uiImage: document.backgroundImage).scaleEffect(zoomScale)
+                    )
+                        .gesture(doubleTapToZoom(in: geometry.size))
+                        
+                    
                     
                     ForEach(document.emojis){ emoji in
                         Text(emoji.text)
-                            .font(font(for: emoji))
+                            .font(animatableWithSize: emoji.fontSize * zoomScale)
                             .position(position(for: emoji, in: geometry.size))
                     }
                 }
@@ -38,6 +43,7 @@ struct DocumentView: View {
                 .onDrop(of: ["public.image","public.text"], isTargeted: nil, perform: { providers, location in
                     var fixedLocation = geometry.convert(location, from: .global)
                 fixedLocation = CGPoint(x: fixedLocation.x - geometry.size.width/2, y: fixedLocation.y - geometry.size.height/2)
+                    fixedLocation = CGPoint(x: fixedLocation.x/zoomScale, y: fixedLocation.y/zoomScale)
                     return self.drop(providers: providers, at: fixedLocation)
                 })
                 
@@ -47,10 +53,8 @@ struct DocumentView: View {
         
         
     }
-    
+    @State private var zoomScale: CGFloat = 1.0
     private let emojiFontSize: CGFloat = 45
-    
-    
     
     private func drop(providers: [NSItemProvider], at location : CGPoint)->Bool{
         var found = providers.loadFirstObject(ofType: URL.self) { url in
@@ -64,11 +68,29 @@ struct DocumentView: View {
         return found
     }
     
-    private func font(for emoji: EmojiArt.Emoji) -> Font{
-        Font.system(size: emoji.fontSize)
-    }
+
     
     private func position(for emoji: EmojiArt.Emoji, in size: CGSize)->CGPoint{
-        CGPoint(x: emoji.location.x + size.width/2, y: emoji.location.y + size.height/2 )
+        var location = emoji.location
+        location = CGPoint(x: location.x * zoomScale, y: location.y * zoomScale)
+        location = CGPoint(x: location.x + size.width/2, y: location.y + size.height/2 )
+        return location
+    }
+    
+    private func zoomToFit(_ image: UIImage?, in size: CGSize){
+        if let image = image, image.size.width > 0, image.size.height>0{
+            let hZoom = size.width / image.size.width
+            let vZoom = size.height / image.size.height
+            zoomScale = min(hZoom, vZoom)
+        }
+    }
+    
+    private func doubleTapToZoom(in size: CGSize) -> some Gesture{
+        TapGesture(count: 2)
+            .onEnded {
+                withAnimation {
+                    zoomToFit(document.backgroundImage, in: size)
+                }
+            }
     }
 }
